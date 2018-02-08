299b37515d933448d8a18a4d8476535f13fe04c4
youngphil <phil@petcity.io>
2018-02-08 15:43:45 +0900

    add ch2

--- src/models.2/WishList.test.js
+++ src/models.2/WishList.test.js
@@ -8,13 +8,12 @@ it('can create a instance of model', () => {
 
   expect(item.price).toBe(499.00)
   expect(item.image).toBe('')
+
+  item.changeName('XBox One')
+  expect(item.name).toBe('XBox One')
 })
 
 it('can cteat a wishist', () => {
-  const item = WishListItem.create({
-    'name': 'PlayStation 4 Pro',
-    'price': 499.00,
-  })
   const list = WishList.create({
     items: [{
       'name': 'PlayStation 4 Pro',
@@ -24,4 +23,19 @@ it('can cteat a wishist', () => {
 
   expect(list.items.length).toBe(1)
   expect(list.items[0].price).toBe(499.00)
+})
+
+it('can add a items', () => {
+  const list = WishList.create()
+  const item = WishListItem.create({
+    'name': 'XBox One',
+    'price': 399.00,
+  })
+  list.add(item)
+
+  expect(list.items.length).toBe(1)
+  expect(list.items[0].name).toBe('XBox One')
+  
+  list.items[0].changeName('Nintendo Switch')
+  expect(list.items[0].name).toBe('Nintendo Switch')
 })
